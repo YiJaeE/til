cd25af1c6211d520b9a9affa3f6f331faea51c3b
yijaee <jinnn2181@gmail.com>
2022-10-07 01:31:45 +0900

    pre-challenge-3-1

---
+++ pre-challenge/pre-challenge-3-1/src/pages/About.tsx
@@ -0,0 +1,19 @@
+import useRouter from '../hooks/useRouter';
+
+const About = () => {
+  const { push } = useRouter();
+
+  const aboutRoot = (e: { preventDefault: () => void }) => {
+    e.preventDefault();
+    push('/');
+  };
+
+  return (
+    <div>
+      <div className="current-location">about</div>
+      <button onClick={aboutRoot}>go main</button>
+    </div>
+  );
+};
+
+export default About;
